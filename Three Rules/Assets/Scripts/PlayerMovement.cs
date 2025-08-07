2eb150102f5e15d555a6c721ef2415eb3f805601
pavle42 <pavlemilovanovic42@gmail.com>
2025-08-07 14:54:31 +0200

    Added PlayerMovement script

---
+++ Three Rules/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private Transform cameraTransform;
+    private float horizontalMovement;
+    private float verticalMovement;
+    private Rigidbody rigidbody;
+    private Vector3 inputDirection;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+
+        inputDirection = GetCameraRelativeInputDirection(horizontalMovement, verticalMovement);
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(transform.position + inputDirection * movementSpeed * Time.fixedDeltaTime);
+
+        if (inputDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(inputDirection, Vector3.up);
+        }
+    }
+
+    Vector3 GetCameraRelativeInputDirection(float horizontal, float vertical)
+    {
+        Vector3 cameraForward = cameraTransform.forward;
+        Vector3 cameraRight = cameraTransform.right;
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        return (cameraForward * verticalMovement + cameraRight * horizontalMovement).normalized;
+    }
+}
